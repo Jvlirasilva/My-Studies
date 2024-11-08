d9c9c07fc33eb31739fcb4b06e0b15d0e82ad2ed
Jvlirasilva <162446102+Jvlirasilva@users.noreply.github.com>
2024-11-08 11:35:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML e CSS?',
+    'HTML é uma linguagem de marcação e CSS de estilização'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem nasce em Tilambuco é?',
+    'Tilambuquense'
+)
+
+criaCartao(
+    'Programação',
+    'Onde se costuma usar HTML e CSS?',
+    'Geralmente na criação e estilização de um site na web'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Pneumoultramicroscopicossilicovulcanoconiose em Inglês?',
+    'Pneumoultramicroscopicossilicovulcanoconiose em ingles é Pneumoultramicroscopicsilicovolcanoconiosis'
+)
